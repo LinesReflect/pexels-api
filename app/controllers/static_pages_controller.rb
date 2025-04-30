d417ecc7170351c16f1e6a06104de3bec5abc965
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-04-30 12:54:04 -0500

    Set the client and collection_id for the home method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @client = Pexels::Client.new
+
+    @collection_id = params[:collection_id] if params[:collection_id].present?
   end
 end
