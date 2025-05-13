c57ae0fbb69114b79c4a5205ecb12be27cd5d620
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-05-13 16:29:49 -0500

    Determine if any of the client's collection id's match the searched id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,12 @@ class StaticPagesController < ApplicationController
     @client = Pexels::Client.new
 
     @collection_id = params[:collection_id] if params[:collection_id].present?
+
+    unless @collection_id.nil?
+      @searched_collection = false
+      @client.collections.all.each do |collection|
+        break @searched_collection = true if collection.id == @collection_id
+      end
+    end
   end
 end
